c1ef8b37819754983d529dec055c3ff83ac97e8f
toewpq <toewpq@gmail.com>
2025-05-23 16:31:17 +0800

    Update logout.js

--- functions/manage/logout.js
+++ functions/manage/logout.js
@@ -1,8 +1,8 @@
 export async function onRequest(context) {
-  return new Response("Logged out.", {
+  return new Response("Logged out", {
     status: 200,
     headers: {
-      "Set-Cookie": `session=; HttpOnly; Secure; Path=/; Max-Age=0`,
+      "Set-Cookie": `session=; Path=/; Max-Age=0`, // 清除 cookie
     },
   });
 }
