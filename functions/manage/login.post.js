fa5650d43e99cdfd6e7dae8bcff6a6781cf37927
toewpq <toewpq@gmail.com>
2025-05-23 16:30:56 +0800

    Create login.post.js

---
+++ functions/manage/login.post.js
@@ -0,0 +1,15 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const { username, password } = await request.json();
+
+  if (username === env.ADMIN_USERNAME && password === env.ADMIN_PASS) {
+    return new Response("ok", {
+      status: 200,
+      headers: {
+        "Set-Cookie": `session=1; HttpOnly; Secure; Path=/; Max-Age=3600`,
+      },
+    });
+  }
+
+  return new Response("Unauthorized", { status: 401 });
+}
