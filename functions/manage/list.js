3f357a130f95bf05da514358d75c4a6af6288e79
toewpq <toewpq@gmail.com>
2025-05-23 16:31:47 +0800

    Update list.js

--- functions/manage/list.js
+++ functions/manage/list.js
@@ -1,6 +1,5 @@
 export async function onRequest(context) {
   const { request, env } = context;
-
   const cookie = request.headers.get("Cookie") || "";
   if (!cookie.includes("session=1")) {
     return new Response("Unauthorized", { status: 401 });
@@ -17,6 +16,6 @@ export async function onRequest(context) {
   const { results } = await stmt.all();
 
   return new Response(JSON.stringify(results), {
-    headers: { "Content-Type": "application/json" }
+    headers: { "Content-Type": "application/json" },
   });
 }
