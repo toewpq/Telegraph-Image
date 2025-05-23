9af0138a70d4f1f74fa751ddcd0597d5fd3c7a42
toewpq <toewpq@gmail.com>
2025-05-23 16:49:05 +0800

    Update login.js

--- functions/manage/login.js
+++ functions/manage/login.js
@@ -1,21 +1,23 @@
 export async function onRequest(context) {
   const { request, env } = context;
-  const url = new URL(request.url);
 
-  const username = url.searchParams.get("u");
-  const password = url.searchParams.get("p");
+  try {
+    const { username, password } = await request.json();
 
-  if (
-    username === env.ADMIN_USERNAME &&
-    password === env.ADMIN_PASS
-  ) {
-    return new Response("ok", {
-      status: 200,
-      headers: {
-        "Set-Cookie": `session=1; HttpOnly; Secure; Path=/; Max-Age=3600`,
-        "Content-Type": "text/plain",
-      },
-    });
+    if (
+      username === env.ADMIN_USERNAME &&
+      password === env.ADMIN_PASS
+    ) {
+      return new Response("ok", {
+        status: 200,
+        headers: {
+          "Set-Cookie": `session=1; HttpOnly; Secure; Path=/; Max-Age=3600`,
+          "Content-Type": "text/plain",
+        },
+      });
+    }
+  } catch (e) {
+    return new Response("Bad Request", { status: 400 });
   }
 
   return new Response("Unauthorized", { status: 401 });
